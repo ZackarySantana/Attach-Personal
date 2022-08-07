c9bba772b3e9c79d76721a23c018f862c31d9c70
ZackarySantana <zackzackyack@gmail.com>
2022-08-07 17:08:14 -0400

    Made default link not redirect to new page

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -50,7 +50,7 @@ const App: Component = () => {
     <>
       <nav class="relative">
         <div class="w-full text-center">
-          <a href="/" target="_blank" class="text-sm text-[#4ea8ed] font-mono">|attach|</a>
+          <a href="/" class="text-sm text-[#4ea8ed] font-mono">|attach|</a>
         </div>
         <Share class="absolute left-[10px] top-[-9px] block sm:hidden" />
         <ThemeSwitch class="absolute right-[10px] top-[-9px] block sm:hidden" />
