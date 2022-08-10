2bdcd03c149dc6e6cb87d53d94c139acdefb43ae
ZackarySantana <zackzackyack@gmail.com>
2022-08-10 10:46:20 -0400

    Moved assets

--- src/config.ts
+++ src/config.ts
@@ -1,14 +1,14 @@
 /* Type Imports - DO NOT TOUCH */
-import { Sections } from "./app/App";
-import { Link } from "./app/links";
-import { Experience } from "./app/experinces";
-import { Images } from "./app/profile";
+import { Sections } from "../src/app/App";
+import { Link } from "../src/app/links";
+import { Experience } from "../src/app/experinces";
+import { Images } from "../src/app/profile";
 /* Type Imports - DO NOT TOUCH */
 
-import profileWebp from "./assets/profile.webp";
-import profilePng from "./assets/profile.png";
-import bannerWebp from "./assets/banner.webp";
-import bannerPng from "./assets/banner.png";
+import profileWebp from "./profile.webp";
+import profilePng from "./profile.png";
+import bannerWebp from "./banner.webp";
+import bannerPng from "./banner.png";
 
 /*
 Both are optional! If you would like neither, just comment out or remove both (but still export images)
@@ -26,7 +26,7 @@ export const images = {
     bannerPng: bannerPng,
 } as Images;
 
-import { getRandomTheme } from "./color_themes";
+import { getRandomTheme } from "../src/color_themes";
 export const colors = getRandomTheme();
 export const random_theme_button = true;
 
@@ -61,14 +61,14 @@ export const skillsList = [
     "Grafana",
 ];
 
-import email from "./assets/icons/email.svg";
-import linkedin from "./assets/icons/linkedin.svg";
-import github from "./assets/icons/github.svg";
-import portfolio from "./assets/icons/portfolio.svg";
-import todo from "./assets/icons/todo.svg";
-import mongodb from "./assets/icons/mongodb.svg";
-import metlife from "./assets/icons/metlife.png";
-import Resume from "./assets/Resume.pdf";
+import email from "./icons/email.svg";
+import linkedin from "./icons/linkedin.svg";
+import github from "./icons/github.svg";
+import portfolio from "./icons/portfolio.svg";
+import todo from "./icons/todo.svg";
+import mongodb from "./icons/mongodb.svg";
+import metlife from "./icons/metlife.png";
+import Resume from "./Resume.pdf";
 
 /**
 There are no required links, but each link must have:
